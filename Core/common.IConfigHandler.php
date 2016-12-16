508836d774ac018f2a8f6e9481a29038dfe857ef
Anton Netterwall <woobione@users.noreply.github.com>
2016-12-16 14:29:57 +0100

    Update common.IConfigHandler.php

--- Core/common.IConfigHandler.php
+++ Core/common.IConfigHandler.php
@@ -13,13 +13,4 @@ interface IConfigHandler extends IConfiguration
 	 * @param string $filePath
 	 */
 	public function AddConfigurationFromFile($filePath);
-
-	/**
-	 * Get the configuration value for $configKey
-	 * Returns false if key doesn't exist
-	 * @param string $configKey
-	 * @param mixed $valueIfNotExists
-	 * @return mixed
-	 */
-	public function Get($configKey, $valueIfNotExists = false);
 }
